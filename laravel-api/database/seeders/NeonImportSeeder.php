a818ca459a9f259f43588f6ab22a3e7e65c55719
Jose <josem.trejoc@gmail.com>
2026-08-20 16:28:16 -0600

    Seeder para importar catalogos de Neon a produccion

---
+++ laravel-api/database/seeders/NeonImportSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
+class NeonImportSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $path = database_path('data_neon.sql');
+        
+        if (!File::exists($path)) {
+            $this->command->error('El archivo data_neon.sql no existe en la carpeta database.');
+            return;
+        }
+
+        $this->command->info('Ejecutando script de importación de Neon DB...');
+        
+        // Ejecutar todo el archivo SQL
+        $sql = File::get($path);
+        DB::unprepared($sql);
+        
+        $this->command->info('¡Importación completada exitosamente!');
+    }
+}
