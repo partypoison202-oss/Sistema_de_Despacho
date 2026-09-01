d79031835b4ff047d079596b5d2e853e2195595b
wichouski <luisvargas0564@gmail.com>
2026-09-01 16:00:32 -0600

    Fix: corregir error de sintaxis (comas faltantes) en DespachoController

--- laravel-api/app/Http/Controllers/API/DespachoController.php
+++ laravel-api/app/Http/Controllers/API/DespachoController.php
@@ -1486,6 +1486,8 @@ class DespachoController extends Controller
                 'informacion_operativa_manana.acople',
                 'informacion_operativa_manana.hora_salida',
                 'informacion_operativa_manana.patio_norte'
+                'informacion_operativa_manana.hora_salida',
+                'informacion_operativa_manana.patio_norte'
             )
             ->orderBy('informacion_operativa_manana.tipo')
             ->orderBy('unidades.numero_eco')
@@ -1509,6 +1511,8 @@ class DespachoController extends Controller
                 'HORA_DE_ACOPLE' => $reg->hora_programada,
                 'HORA_PROGRAMADA' => $reg->hora_programada,
                 'ACOPLE' => $reg->acople,
+                'HORA_SALIDA' => $reg->hora_salida,
+                'PATIO_NORTE' => filter_var($reg->patio_norte, FILTER_VALIDATE_BOOLEAN)
                 'HORA_SALIDA' => $reg->hora_salida,
                 'PATIO_NORTE' => filter_var($reg->patio_norte, FILTER_VALIDATE_BOOLEAN)
             ];
@@ -1553,6 +1557,8 @@ class DespachoController extends Controller
                 "{$tableName}.acople",
                 "{$tableName}.hora_salida",
                 "{$tableName}.patio_norte"
+                "{$tableName}.hora_salida",
+                "{$tableName}.patio_norte"
             )
             ->orderBy("{$tableName}.tipo")
             ->orderBy('unidades.numero_eco')
@@ -1576,6 +1582,8 @@ class DespachoController extends Controller
                 'HORA_DE_ACOPLE' => $reg->hora_programada,
                 'HORA_PROGRAMADA' => $reg->hora_programada,
                 'ACOPLE' => $reg->acople,
+                'HORA_SALIDA' => $reg->hora_salida,
+                'PATIO_NORTE' => filter_var($reg->patio_norte, FILTER_VALIDATE_BOOLEAN)
                 'HORA_SALIDA' => $reg->hora_salida,
                 'PATIO_NORTE' => filter_var($reg->patio_norte, FILTER_VALIDATE_BOOLEAN)
 
@@ -1903,6 +1911,7 @@ class DespachoController extends Controller
             
             if ($unidadReemplazo) {
                 $conductorReemplazo = DB::table('conductores')->where('tarjeton', $tarjetonReemplazo)->first();
+                $nombreConductorReemplazo = $conductorReemplazo ? trim(($conductorReemplazo->nombres ?? '') . ' ' . ($conductorReemplazo->apellidos ?? '')) : null;
                 $nombreConductorReemplazo = $conductorReemplazo ? trim(($conductorReemplazo->nombres ?? '') . ' ' . ($conductorReemplazo->apellidos ?? '')) : null;
 
                 // Desasignar cualquier otra unidad que tenga este tarjetón
